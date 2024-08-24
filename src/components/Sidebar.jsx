ab37e30afad0fe090877a3fb2299c2b1b2263cad
balolot.johnrey <balolot.johnrey@gmail.com>
2024-08-24 21:15:36 +0800

    initial react

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+
+function Sidebar() {
+  return (
+    <>
+    <div className="sidebar">
+    <div className="titlebar">OVERBLOCK</div>
+    This is a side bar
+    </div>
+    </>
+  )
+}
+
+export default Sidebar;
