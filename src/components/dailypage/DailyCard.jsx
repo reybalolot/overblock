237c980a59e95c8f63d357d2d0867d3cc1966644
balolot.johnrey <balolot.johnrey@gmail.com>
2024-10-09 23:00:52 +0800

    daily: added grid

--- src/components/dailypage/DailyCard.jsx
+++ src/components/dailypage/DailyCard.jsx
@@ -1,7 +1,7 @@
 const DailyCard = ({content, colStart}) => {
     return (
         <>
-            <div className="max-w-pros h-[12rem] py-1 text-center text-white text-3xl rounded overflow-hidden border border-gray-500 hover:bg-slate-500 anim-300"
+            <div className="max-w-pros h-[12rem] min-w-[16rem] py-1 text-center text-white text-3xl rounded overflow-hidden border border-gray-500 hover:bg-slate-500 anim-300 snap-start"
                 style={{gridColumnStart:colStart}}>
                 {content}
             </div>
--- src/pages/DailyPage.jsx
+++ src/pages/DailyPage.jsx
@@ -4,18 +4,30 @@ import DailyCard from "../components/dailypage/DailyCard";
 const DailyPage = () => {
 
     let cardArray = []
-    for (let i = 0; i < 168; i++) {
-        cardArray.push(<DailyCard key={i}/>)
+    for (let i = 0; i < 24; i++) {
+        cardArray.push(
+            <>
+                <div className="w-[200%] flex flex-row justify-start">
+                    <DailyCard key={i} content={i}/>
+                    <DailyCard key={i+1} content={i+1}/>
+                    <DailyCard key={i+2} content={i+2}/>
+                    <DailyCard key={i+3} content={i+3}/>
+                    <DailyCard key={i+4} content={i+4}/>
+                    <DailyCard key={i+5} content={i+5}/>
+                    <DailyCard key={i+6} content={i+6}/>
+                </div>
+            </>
+        )
     }
 
     return (
         <>
-        <div className="w-full max-h-full my-2 me-2 rounded-md overflow-y-scroll bg-primary snap-mandatory snap-y">
-            {/* <div className="w- text-white px-4 fixed bg-red-500 rounded-ss-md">WEEK GOES HERE</div> */}
+        <div className="w-full max-h-full my-2 me-2 rounded-md overflow-y-scroll no-scrollbar bg-primary snap-mandatory snap-y">
+            <div className="w- text-white px-4 snap-start bg-red-500 rounded-ss-md">WEEK GOES HERE</div>
             <div className="flex flex-row mt-6">
                 <HourRule/>
-                <div className="flex flex-col w-full h-full">
-                    <div className="w-full grid grid-rows-24 grid-cols-7 gap-1 overflow-hidden">
+                <div className="flex flex-col h-full w-full">
+                    <div className="max-w-full grid grid-rows-24 grid-cols-1 gap-1 overflow-x-scroll no-scrollbar snap-mandatory snap-x">
                         {cardArray}
                     </div>
                 </div>
